330469977de8200c79bdefcb5d36262a315e4861
Carlos Hugo Lima Lopes <chlopes@banpara.com>
2018-11-06 15:07:24 -0300

    Iniciando CRUD de Pessoa com Entity Framework e Migration

--- FinancasCasal/Startup.cs
+++ FinancasCasal/Startup.cs
@@ -32,7 +32,8 @@ namespace FinancasCasal {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
     services.AddDbContext<FinancasCasalContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("FinancasCasalContext")));
+            options.UseMySql(Configuration.GetConnectionString("FinancasCasalContext"), builder => 
+            builder.MigrationsAssembly("FinancasCasal")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
