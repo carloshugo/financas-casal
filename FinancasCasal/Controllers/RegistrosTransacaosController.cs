0f3e63bd757bb90289c881584bdd406251bd18b6
Hugo <carloshugolopes@gmail.com>
2018-11-11 13:10:16 -0200

    Preparando a navegação de buscas de transações

---
+++ FinancasCasal/Controllers/RegistrosTransacaosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancasCasal.Controllers
+{
+    public class RegistrosTransacaosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaPorGrupo()
+        {
+            return View();
+        }
+    }
+}
